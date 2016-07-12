d6a3e7e18dedf32ae0fd987127824db9b3a16b06
matutter <matutter4@gmail.com>
2016-07-11 21:05:55 -0400

    including istanbul

--- test/access.js
+++ test/access.js
@@ -2,8 +2,10 @@ var assert = require('chai').use(require('chai-as-promised')).assert
 var pro = require('..')
 var fs = require('fs')
 
-var dir = './files'
-var filename = './files/access.txt'
+var dir = './test/files'
+var filename = './test/files/access.txt'
+var ENOENT_filename = './test/files/missing.txt'
+
 
 describe('setup', ()=> {
   it('should make a "files" folder', ()=> {
@@ -13,17 +15,32 @@ describe('setup', ()=> {
     var fd = fs.openSync(filename, 'w')
     fs.writeSync(fd, 'test data', 0, 'utf8')
     fs.closeSync(fd)
-
   })
 })
 
 
 describe('access', ()=> {
+
   describe('without options', ()=> {
     it('should default to R_OK', ()=> {
-	return assert.eventually.equal(pro.access(filename), null)
+      return assert.eventually.equal(pro.access(filename), null)
     })
   })
+
+  describe('on a file that doesn\'t exist', ()=> {
+    it('should throw ENOENT', ()=> {
+      var p = pro.access(ENOENT_filename).catch(e => e.code)
+      return assert.eventually.equal(p, "ENOENT")
+    })
+  })
+
 })
 
-
+describe('cleanup', ()=> {
+  it('should remove the r+w file', ()=> {
+    fs.unlinkSync(filename)
+  })
+  it('should remove "files" folder', ()=> {
+    fs.rmdirSync(dir)
+  })
+})
